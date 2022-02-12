ef0f668f358eb6c97f67bc351c5bb70d4cfd2c52
Viral Mistry <viralhmistry@gmail.com>
2022-02-12 21:32:09 +0530

    Add root action

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+
+  def index
+    successful_response('Open Api Specification Demo')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "home#index"
   namespace :v1 do
     resources :books
   end
